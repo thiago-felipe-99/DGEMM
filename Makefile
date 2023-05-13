d48c2ff31b12712e5bc579329f297cf79126ba83
Thiago Felipe <thiago.felipe@tutanota.com>
2023-05-13 16:18:19 -0300

    Criando programa em python

---
+++ Makefile
@@ -0,0 +1,8 @@
+python:
+	python main.py 16
+	python main.py 32
+	python main.py 64
+	python main.py 128
+	python main.py 256
+	python main.py 512
+	python main.py 1024
---
+++ main.py
@@ -0,0 +1,39 @@
+import random
+import sys
+import time
+
+
+def get_matrix_lenght():
+    arg1 = sys.argv[1]
+    return int(arg1)
+
+
+def generate_matrix(lenght):
+    m_a = [[random.random() for _ in range(lenght)] for _ in range(lenght)]
+    m_b = [[random.random() for _ in range(lenght)] for _ in range(lenght)]
+
+    return m_a, m_b
+
+
+def multiply_matrix(matrix_a, matrix_b, lenght):
+    matrix_c = [[0 for _ in range(lenght)] for _ in range(lenght)]
+
+    for i in range(lenght):
+        for j in range(lenght):
+            for k in range(lenght):
+                matrix_c[i][j] += matrix_a[i][k] * matrix_b[k][j]
+
+    return matrix_c
+
+
+def main():
+    matrix_lenght = get_matrix_lenght()
+    matrix_a, matrix_b = generate_matrix(matrix_lenght)
+    init = time.perf_counter()
+    _ = multiply_matrix(matrix_a, matrix_b, matrix_lenght)
+    final = time.perf_counter()
+    print(f"Time to calculate matrix: {final - init:0.4f}s")
+
+
+if __name__ == "__main__":
+    main()
