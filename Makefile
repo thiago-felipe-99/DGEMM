7af5234f701d1d814424a99affd372da42fb22a7
Thiago Felipe <thiago.felipe@tutanota.com>
2023-05-13 19:04:57 -0300

    Criando dgemm para avx512

--- Makefile
+++ Makefile
@@ -24,5 +24,14 @@ dgemm_avx256:
 	./out/dgemm_avx256 2048
 	# ./out/dgemm_avx256 4096
 
+dgemm_avx512:
+	gcc -mavx512f -o out/dgemm_avx512 dgemm_avx512.c
+	./out/dgemm_avx512 128
+	./out/dgemm_avx512 256
+	./out/dgemm_avx512 512
+	./out/dgemm_avx512 1024
+	./out/dgemm_avx512 2048
+	# ./out/dgemm_avx512 4096
+
 clean:
 	rm -r ./out/*
---
+++ dgemm_avx512.c
@@ -0,0 +1,99 @@
+#include <errno.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <x86intrin.h>
+
+#define EXIT_FAILURE 1
+
+#define AVX_QT_DOUBLE 8
+#define AVX_SIZE_DOUBLE 64
+
+const double range = 4096;
+
+void multiplyMatrix(int lenght, double *matrixA, double *matrixB,
+                    double *matrixC) {
+  for (int i = 0; i < lenght; i += AVX_QT_DOUBLE) {
+    for (int j = 0; j < lenght; j++) {
+      // cij = C[i][j]
+      __m512d c0 = _mm512_load_pd(matrixC + i + j * lenght);
+
+      for (int k = 0; k < lenght; k++) {
+        // cij += A[i][k]*B[k][j]
+        c0 = _mm512_add_pd(
+            c0, _mm512_mul_pd(_mm512_load_pd(matrixA + i + k * lenght),
+                              _mm512_broadcastsd_pd(
+                                  _mm_load_sd(matrixB + k + j * lenght))));
+      }
+
+      _mm512_store_pd(matrixC + i + j * lenght, c0);
+    }
+  }
+}
+
+int getMatrixLength(int argc, char *argv[]) {
+  int length;
+  char *endptr;
+
+  if (argc != 2) {
+    fprintf(stderr, "Error: Invalid number of arguments\n");
+    return EXIT_FAILURE;
+  }
+
+  errno = 0;
+
+  long int_val = strtol(argv[1], &endptr, 10);
+
+  if (errno != 0 || *endptr != '\0') {
+    fprintf(stderr, "Error: Invalid input\n");
+    return EXIT_FAILURE;
+  }
+
+  if (int_val < INT_MIN || int_val > INT_MAX) {
+    fprintf(stderr, "Error: Input out of range\n");
+    return EXIT_FAILURE;
+  }
+
+  length = (int)int_val;
+
+  return length;
+}
+
+void generateRandonsMatrix(int length, double *matrixA, double *matrixB,
+                           double *matrixC) {
+  srand(time(NULL));
+
+  for (int index = 0; index < length * length; index++) {
+    matrixA[index] = ((double)rand() / RAND_MAX) * range;
+    matrixB[index] = ((double)rand() / RAND_MAX) * range;
+    matrixC[index] = 0;
+  }
+}
+
+int main(int argc, char *argv[]) {
+  int length = getMatrixLength(argc, argv);
+
+  double *matrixA =
+      aligned_alloc(AVX_SIZE_DOUBLE, length * length * sizeof(double));
+  double *matrixB =
+      aligned_alloc(AVX_SIZE_DOUBLE, length * length * sizeof(double));
+  double *matrixC =
+      aligned_alloc(AVX_SIZE_DOUBLE, length * length * sizeof(double));
+
+  generateRandonsMatrix(length, matrixA, matrixB, matrixC);
+
+  clock_t start = clock(), diff;
+  multiplyMatrix(length, matrixA, matrixB, matrixC);
+  diff = clock() - start;
+
+  int mseconds = diff * 1000 / CLOCKS_PER_SEC;
+  printf("Time to calculate matrix %dx%d: %dms\n", length, length, mseconds);
+
+  free(matrixA);
+  free(matrixB);
+  free(matrixC);
+
+  return 0;
+}
